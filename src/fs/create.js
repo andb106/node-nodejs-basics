8afac24db860acd7adc16e09dbc484243aa38c39
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-14 15:37:39 +0300

    feat: add solition for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+
+        const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+
+        await writeFile(pathToFile, 'I am fresh and young', { flag: 'ax' });
+
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
 };
 
 await create();
