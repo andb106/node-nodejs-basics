d8bbc4cee4b67002409e156ccdc7d33d0ec08d65
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-16 14:25:52 +0300

    feat: add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    const res = [];
+    const searchKey = 'RSS_';
+    const variables = process.env;
+
+    Object.keys(variables).forEach((item) => {
+        if (item.startsWith(searchKey)) {
+           res.push(`${item}=${variables[item]}`);
+        }
+    });
+
+    if (res.length) {
+        console.log(res.join('; '));
+    }
 };
 
 parseEnv();
