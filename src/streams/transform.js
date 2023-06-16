e75e5e0b93543dc56ee9a3452c2d6a3a21c266ce
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-16 19:15:50 +0300

    feat: add solution for streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { pipeline } from 'node:stream/promises';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformReverse = new Transform({
+        transform(chunk, encoding, callback) {
+          const data = chunk.toString().trim().split('').reverse().join('') + '\n';
+          callback(null, data);
+        },
+      });
+
+    pipeline(
+        process.stdin,
+        transformReverse,
+        process.stdout
+    )
 };
 
 await transform();
