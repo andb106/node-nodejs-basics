8b53c1a1ae5e9989d9cb6536fdabd42dd4602034
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-16 21:45:29 +0300

    feat: add solution for compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const pathSource = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const pathDestination = path.join(__dirname, 'files', 'archive.gz');
+
+    const source = createReadStream(pathSource);
+    const destination = createWriteStream(pathDestination);
+
+    const gzip = createGzip();
+
+    try {
+        await pipeline(source, gzip, destination);
+    } catch (error) {
+        console.error('error!', error);
+        process.exitCode = 1;
+    }
+
 };
 
 await compress();
