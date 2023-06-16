63e27a1c6f3ad1491124d2d438cced5a4ce1804d
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-16 14:51:25 +0300

    feat: add solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const res = [];
+    const searchKey = '--';
+    const cliArguments = process.argv.slice(2);
+
+    cliArguments.forEach((item, i) => {
+        if (item.startsWith(searchKey)) {
+            const prop = item.replace(searchKey, '');
+            const value = cliArguments[i + 1];
+            res.push(`${prop} is ${value}`);
+        }
+    })
+    console.log(res.join(', '));
 };
 
 parseArgs();
