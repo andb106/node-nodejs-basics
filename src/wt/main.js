e3af75b93d4673e67ccb6dc47ae711b759a9d8dd
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-17 12:05:40 +0300

    feat: add solution for worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const pathToFile = path.join(__dirname, 'worker.js');
+
+    const createWorker =  (data) => {
+        return new Promise((resolve, reject) => {
+        const worker = new Worker(pathToFile, { workerData: data });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        }
+    )}
+
+    const numberCPU = cpus().length;
+    const increment = 10;
+
+    const workersArray = Array(numberCPU).fill().map((_, index) => createWorker(index + increment));
+
+    try {
+        const res = await Promise.allSettled(workersArray);
+        console.log(res.map((item) => item.value));
+    } catch (error) {
+        console.log('error from Promise.allSettled', error)
+    }
 };
+
 
 await performCalculations();
