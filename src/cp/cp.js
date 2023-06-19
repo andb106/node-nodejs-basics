56ad57fde7343f4cb637303e88afd0bb9d5bfbb0
Andrey Boytsov <po4ta_notice@mail.ru>
2023-06-19 21:54:25 +0300

    refactor: in cp.js change spawn to fork

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,13 +1,13 @@
 import path from 'node:path';
 import { fileURLToPath } from 'url';
-import { spawn } from 'node:child_process';
+import { spawn, fork } from 'node:child_process';
 
 const spawnChildProcess = async (args) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const pathToFile = path.join(__dirname, 'files', 'script.js');
 
-    const child = spawn('node', [pathToFile, ...args], {stdio: [process.stdin, process.stdout, process.stderr, 'ipc']});
+    const child = fork(pathToFile, args);
 
 };
 
